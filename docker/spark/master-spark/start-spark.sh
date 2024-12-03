e3d34c146daa131a2a69335a5def287c67769a8b
Kimwonjoon <kimpass189@gmail.com>
2024-12-03 15:49:34 +0900

    worker 두개 나오던거 해결

--- docker/spark/master-spark/start-spark.sh
+++ docker/spark/master-spark/start-spark.sh
@@ -5,7 +5,7 @@ set -e
 $SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master &
 
 # Spark worker 노드 실행 (현재 컨테이너를 워커로 사용)
-$SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker $SPARK_MASTER_URL &
+#$SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker $SPARK_MASTER_URL &
 
 # 컨테이너가 종료되지 않도록 대기
 tail -f /dev/null
